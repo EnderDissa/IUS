8f3e4f42ad4bd8e7a0565259fa0c40d2b1d423ab
EnderDIssa <54418849+EnderDIssa@users.noreply.github.com>
2025-05-05 15:06:47 +0300

    added visualizer base

---
+++ front/visualizer.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+df = pd.read_csv('input.csv')
+#print(df.columns)
+
+df.sort_values(by='time', inplace=True)
+processes = df['process_id'].unique()
+
+plt.figure(figsize=(12, 6))
+
+for process in processes:
+    process_data = df[df['process_id'] == process]
+    #print(process_data)
+    for i in range(len(process_data) - 1):
+        current = process_data.iloc[i]
+        next_event = process_data.iloc[i + 1]
+        if current['status'] == 1:
+            start_time = current['time']
+            end_time = next_event['time']
+            plt.barh(process, end_time - start_time, left=start_time, height=0.1, color='green', edgecolor='black')
+
+plt.title('Гонка процессов')
+plt.xlabel('Время')
+plt.ylabel('ID процесса')
+plt.grid(True, axis='x', linestyle='--', alpha=0.5)
+
+plt.tight_layout() 
+plt.yticks(processes)
+plt.tight_layout()
+plt.gcf().canvas.manager.set_window_title('Визуализатор гонки процессов')
+
+now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+plt.savefig(f"process_race_{now}.png", dpi=300)
+
+
+plt.show()
